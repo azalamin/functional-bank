c20c8bd7633c16e992047256743d79780d2c4aa0
Al-amin Sheikh <azalamins@gmail.com>
2022-02-11 23:20:45 +0600

    updated

--- js/transaction.js
+++ js/transaction.js
@@ -2,30 +2,74 @@ function getInputValue(id) {
     const inputField = document.getElementById(id);
     const amount = parseFloat(inputField.value);
     // Clear deposit input
-    amount.value = '';
+    inputField.value = '';
     return amount;
 }
+// update all balance
+function updateTotalField(id, amount) {
+    const balanceText = document.getElementById(id);
+    const balance = parseFloat(balanceText.innerText);
+    balanceText.innerText = balance + amount;
+}
 
-document.getElementById('deposit-btn').addEventListener('click', function() {
+// update main balance
+function updateMainBalance(depositAmount, isAdd) {
+    const mainBalanceText = document.getElementById('balance');
+    const mainBalance = parseFloat(mainBalanceText.innerText);
+    if (isAdd) {
+        mainBalanceText.innerText = mainBalance + depositAmount;
+    } else {
+        mainBalanceText.innerText = mainBalance - depositAmount;
+    }
+}
+
+// deposit and update balance
+function depositBalance() {
+    // update deposit balance
     const depositAmount = getInputValue('deposit-input');
-    const depositText = document.getElementById('deposit');
-    const depositBalance = parseFloat(depositText.innerText);
-    const mainBalanceText = document.getElementById('balance');
-    // Convert string to number
-    const mainBalance = parseFloat(mainBalanceText.innerText);
-    // Update Deposit balance and main balance
-    depositText.innerText = depositBalance + depositAmount;
-    mainBalanceText.innerText = mainBalance + depositAmount;
-});
+    if (depositAmount > 0) {
+        // update after deposit balance
+        updateTotalField('deposit', depositAmount);
+        //update main balance
+        updateMainBalance(depositAmount, true);
+    }
+}
 
-document.getElementById('withdraw-btn').addEventListener('click', function() {
-    const withdrawInputBalance = getInputValue('withdraw-input')
-    const withdrawBalanceText = document.getElementById('withdraw');
+// withdraw and update balance
+function withdrawBalance() {
+    // update withdraw balance
+    const withdrawInputBalance = getInputValue('withdraw-input');
     const mainBalanceText = document.getElementById('balance');
-    // Convert string to number
-    const withdrawBalance = parseFloat(withdrawBalanceText.innerText);
     const mainBalance = parseFloat(mainBalanceText.innerText);
-    // Update withdraw balance and main balance
-    withdrawBalanceText.innerText = withdrawBalance + withdrawInputBalance;
-    mainBalanceText.innerText = mainBalance - withdrawInputBalance;
-})
+    if (withdrawInputBalance > 0) {
+        if (mainBalance > withdrawInputBalance) {
+            // update after withdraw balance
+            updateTotalField('withdraw', withdrawInputBalance);
+            // update main balance
+            updateMainBalance(withdrawInputBalance, false);
+        } else {
+            alert("You don't have sufficient balance or your number is negative")
+        }
+    } else {
+        alert("Input cannot be negative!!")
+    }
+}
+
+// document.getElementById('deposit-btn').addEventListener('click', function() {
+//     // update deposit balance
+//     const depositAmount = getInputValue('deposit-input');
+//     // update after deposit balance
+//     updateTotalField('deposit', depositAmount);
+//     //update main balance
+//     updateMainBalance(depositAmount, true);
+
+// });
+
+// document.getElementById('withdraw-btn').addEventListener('click', function() {
+//     // update withdraw balance
+//     const withdrawInputBalance = getInputValue('withdraw-input');
+//     // update after withdraw balance
+//     updateTotalField('withdraw', withdrawInputBalance)
+//         // update main balance
+//     updateMainBalance(withdrawInputBalance, false)
+// })
